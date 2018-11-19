49290cd5eb8ee4fa5a2383332fe85d020506bb61
Gonzalo Lopez <gonzalo.lopez@digitalonus.com>
2018-11-19 14:44:33 -0600

    ADD Docker Stack Deploy functions

--- vars/stackRollout.groovy
+++ vars/stackRollout.groovy
@@ -1,5 +1,5 @@
-def call(stackFile, stackName){
-  def IP = ""
+def call(stackName, stackFile){
+  def String IP = ""
   dir("terraform") {
     IP = sh(script:'terraform output docker-ip',
         returnStdout: true).trim()
@@ -7,12 +7,12 @@ def call(stackFile, stackName){
   dir("app"){
     sshagent(credentials: ['ssh-private-key']) {
       sh """ssh-add -k ${env.TF_VAR_my_private_key_path} """
+      sh """scp -o StrictHostKeyChecking=no \
+       ${stackFile} ubuntu@${IP}:/tmp"""
+
       sh """ssh -o StrictHostKeyChecking=no \
-         -l ubuntu -NL 2340:/var/run/docker.sock ${IP} & """
-      sh """DOCKER_HOST=tcp://localhost:2340 \
-            docker stack deploy -c \
-            ${stackFile} ${stackName} """ 
-      }
+          ubuntu@${IP} docker stack deploy -c \
+          /tmp/${stackFile} ${stackName} """ 
     }
   }
 }
