748b2ad087bda37ae925c83d1fd1858639f23d08
Gonzalo Lopez <gonzalo.lopez@digitalonus.com>
2018-11-19 14:13:56 -0600

    FIX ssh-agent issue

--- vars/stackRollout.groovy
+++ vars/stackRollout.groovy
@@ -1,4 +1,4 @@
-def call(stack){
+def call(stackFile, stackName){
   def IP = ""
   dir("terraform") {
     IP = sh(script:'terraform output docker-ip',
@@ -6,10 +6,13 @@ def call(stack){
   }
   dir("app"){
     sshagent(credentials: ['ssh-private-key']) {
-      sh """ ssh-add -k ${env.TF_VAR_my_private_key_path} """
+      sh """ssh-add -k ${env.TF_VAR_my_private_key_path} """
       sh """ssh -o StrictHostKeyChecking=no \
-         -l ubuntu ${IP} \
-           uname -a"""
+         -l ubuntu -NL 2340:/var/run/docker.sock ${IP} & """
+      withEnv(['DOCKER_HOST=tcp://localhost:2340']) {
+        sh """docker stack deploy -c \
+           ${stackFile} ${stackName} """ 
+      }
     }
   }
 }
