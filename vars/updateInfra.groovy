22d923c49e302a937a84531acd360cec0c7b48e2
Gonzalo Lopez <gmlp.24a@gmail.com>
2018-11-21 15:25:15 -0600

    ADD updateInfra funciton

--- vars/updateInfra.groovy
+++ vars/updateInfra.groovy
@@ -1,5 +1,6 @@
 def call(){
     dir("terraform"){
+        sh 'terraform init -input=false'
         sh 'terraform plan -out=plan -input=false'
         input(message: "Do you want to apply this plan?", ok: "yes")
         sh 'terraform apply -input=false plan'
