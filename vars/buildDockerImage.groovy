3164c1b4798f8e7ff109542131ef3d1b8d4c0209
Gonzalo Lopez <gmlp.24a@gmail.com>
2018-11-23 12:38:18 -0600

    ADD nexus support

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,11 +1,14 @@
 def call(String image, String sonarServer){
   def String tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}".replace("/","-")
   dir ("app"){
-    sh """docker image build \
-        --build-arg SONAR_TOKEN=${env.SONAR_TOKEN} \
-        --build-arg SONAR_SERVER=${sonarServer} \
-        --build-arg BRANCH_NAME=${env.BRANCH_NAME} \
-          -t ${image}:${tagBeta} ."""
+    configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
+        sh "cp $MAVEN_SETTINGS settings.xml"
+        sh """docker image build \
+            --build-arg SONAR_TOKEN=${env.SONAR_TOKEN} \
+            --build-arg SONAR_SERVER=${sonarServer} \
+            --build-arg BRANCH_NAME=${env.BRANCH_NAME} \
+              -t ${image}:${tagBeta} ."""
+    }
   }
   
   withCredentials([usernamePassword(
