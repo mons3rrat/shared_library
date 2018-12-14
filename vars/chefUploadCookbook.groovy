8c10515e71229eb90fe5911a525895d1362b17d9
Gonzalo Lopez <gmlp.24a@gmail.com>
2018-12-14 12:58:20 -0600

    REF chef upload cookbook

--- vars/chefUploadCookbook.groovy
+++ vars/chefUploadCookbook.groovy
@@ -1,7 +1,9 @@
-def call(String name, String path="../"){
+def call(String name, String path="./"){
   withCredentials([file(credentialsId: 'chef-client-key', variable: 'KEY' )]) {
     configFileProvider([configFile(fileId: 'knife-config', variable: 'KNIFE_CONFIG')]) {
-      sh "knife cookbook upload ${name} -o ${path} -c ${KNIFE_CONFIG} -k ${KEY}"
+      sh "mkdir ./cookbooks"
+      sh "mv * cookbooks"
+      sh "knife cookbook upload -o ${path} -c ${KNIFE_CONFIG} -k ${KEY} ${name}"
     }
   }
 }
