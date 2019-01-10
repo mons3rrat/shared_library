9dbd43bca37cb6a10060fb60112ff975a0557222
Gonzalo Lopez <gmlp.24a@gmail.com>
2019-01-10 10:37:27 -0600

    ADD support for complex stacks

--- vars/stackRollout.groovy
+++ vars/stackRollout.groovy
@@ -1,4 +1,4 @@
-def call(String stackName, String stackFile){
+def call(String stackName, String stackFile, String stackDir=''){
   def String IP = ""
   dir("terraform") {
     IP = sh(script:'terraform output docker-ip',
@@ -7,12 +7,26 @@ def call(String stackName, String stackFile){
   dir("app"){
     sshagent(credentials: ['ssh-private-key']) {
       sh """ssh-add -k ${env.TF_VAR_my_private_key_path} """
-      sh """scp -o StrictHostKeyChecking=no \
-       ${stackFile} ubuntu@${IP}:/tmp"""
+      if(stackDir != ''){
+        sh "tar -czf stacks.tar.gz ${stackDir}"
 
-      sh """ssh -o StrictHostKeyChecking=no \
-          ubuntu@${IP} docker stack deploy -c \
-          /tmp/${stackFile} ${stackName} """ 
+        sh """scp -o StrictHostKeyChecking=no \
+            stacks.tar.gz ubuntu@${IP}:/tmp"""
+
+        sh """ssh -o StrictHostKeyChecking=no \
+            ubuntu@${IP} tar -C /tmp -xzvf /tmp/stacks.tar.gz""" 
+
+        sh """ssh -o StrictHostKeyChecking=no \
+            ubuntu@${IP} docker stack deploy -c \
+            /tmp/${stackDir}/${stackFile} ${stackName} """ 
+      }
+      else {
+        sh """scp -o StrictHostKeyChecking=no \
+        ${stackFile} ubuntu@${IP}:/tmp"""
+        sh """ssh -o StrictHostKeyChecking=no \
+            ubuntu@${IP} docker stack deploy -c \
+            /tmp/${stackFile} ${stackName} """ 
+      }
     }
   }
 }
