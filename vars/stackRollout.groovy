a4aa4f2cf233d26e0b7496d59f0eca956bd4f76a
Gonzalo Lopez <gmlp.24a@gmail.com>
2019-01-10 12:33:20 -0600

    [FIX]stackRollout: wrong path

--- vars/stackRollout.groovy
+++ vars/stackRollout.groovy
@@ -7,19 +7,17 @@ def call(String stackName, String stackFile, String stackDir=''){
   sshagent(credentials: ['ssh-private-key']) {
     sh """ssh-add -k ${env.TF_VAR_my_private_key_path} """
     if(stackDir != ''){
-      dir(stackDir){
-        sh "tar -czf stacks.tar.gz ${stackDir}"
+      sh "tar -czf stacks.tar.gz ${stackDir}"
 
-        sh """scp -o StrictHostKeyChecking=no \
-            stacks.tar.gz ubuntu@${IP}:/tmp"""
+      sh """scp -o StrictHostKeyChecking=no \
+          stacks.tar.gz ubuntu@${IP}:/tmp"""
 
-        sh """ssh -o StrictHostKeyChecking=no \
-            ubuntu@${IP} tar -C /tmp -xzvf /tmp/stacks.tar.gz""" 
+      sh """ssh -o StrictHostKeyChecking=no \
+          ubuntu@${IP} tar -C /tmp -xzvf /tmp/stacks.tar.gz""" 
 
-        sh """ssh -o StrictHostKeyChecking=no \
-            ubuntu@${IP} docker stack deploy -c \
-            /tmp/${stackDir}/${stackFile} ${stackName} """ 
-      }
+      sh """ssh -o StrictHostKeyChecking=no \
+          ubuntu@${IP} docker stack deploy -c \
+          /tmp/${stackDir}/${stackFile} ${stackName} """ 
     }
     else {
       dir("app"){
